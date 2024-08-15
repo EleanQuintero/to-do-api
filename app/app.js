0e2d70e8a0f5ad5fb584c18471ed20fa265091d6
Elean Quintero <167714130+EleanQuintero@users.noreply.github.com>
2024-08-16 01:11:17 +0200

    updating port

--- app/app.js
+++ app/app.js
@@ -9,7 +9,7 @@ app.use(express.json())
 app.disable('x-powered-by')
 
 app.use('/todos', todosRouter)
-const PORT = process.env.PORT ?? 4567
+const PORT = process.env.PORT || 4567
 
 app.listen(PORT, () => {
   console.log(`Server listening on Port: http://localhost:${PORT}`)

25b18344b962936039b8504b0fb6f32aad96ab6e
Elean Quintero <167714130+EleanQuintero@users.noreply.github.com>
2024-08-16 02:04:37 +0200

    refactor

--- models/My-SQL/todos.js
+++ models/My-SQL/todos.js
@@ -1,6 +1,14 @@
 import { response } from 'express'
 import mysql from 'mysql2/promise'
-import { config } from './config.js'
+import { DB_DATABASE, DB_HOST, DB_PASSWORD, DB_PORT, DB_USER } from './config.js'
+
+const config = {
+  host: DB_HOST,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  port: DB_PORT,
+  database: DB_DATABASE
+}
 
 const connection = mysql.createConnection(config)
 

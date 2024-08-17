4030758d9018717c435caadaa8876cffd2d0b34e
Elean Quintero <167714130+EleanQuintero@users.noreply.github.com>
2024-08-17 16:30:18 +0200

    cambios en las variables de entorno

--- models/My-SQL/config.js
+++ models/My-SQL/config.js
@@ -2,12 +2,4 @@ export const DB_HOST = process.env.DB_HOST || 'localhost'
 export const DB_USER = process.env.DB_USER || 'root'
 export const DB_PASSWORD = process.env.DB_PASSWORD || ''
 export const DB_PORT = process.env.DB_PORT || 3306
-export const DB_DATABASE = process.env.DB_DATABASE || 'TodosDB'
-
-export const config = {
-  host: DB_HOST,
-  user: DB_USER,
-  password: DB_PASSWORD,
-  port: DB_PORT,
-  database: DB_DATABASE
-}
+export const DB_NAME = process.env.DB_DATABASE || 'TodosDB'
--- models/My-SQL/todos.js
+++ models/My-SQL/todos.js
@@ -1,13 +1,13 @@
 import { response } from 'express'
 import mysql from 'mysql2/promise'
-import { DB_DATABASE, DB_HOST, DB_PASSWORD, DB_PORT, DB_USER } from './config.js'
+import { DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER } from './config.js'
 
 const config = {
   host: DB_HOST,
   user: DB_USER,
   password: DB_PASSWORD,
   port: DB_PORT,
-  database: DB_DATABASE
+  database: DB_NAME
 }
 
 const connection = mysql.createConnection(config)
